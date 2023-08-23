eeea1e1627b346c4a88c059fab8b9ce2186a9802
kmlefran <kgagnon@lakeheadu.ca>
2023-08-23 16:10:01 -0400

    Created trial Aimqb inp data

--- aiida_aimall/data/__init__.py
+++ aiida_aimall/data/__init__.py
@@ -11,39 +11,65 @@ from aiida.orm import Dict
 
 # A subset of diff's command line options
 cmdline_options = {
-    Optional("ignore-case"): bool,
-    Optional("ignore-file-name-case"): bool,
-    Optional("ignore-tab-expansion"): bool,
-    Optional("ignore-space-change"): bool,
-    Optional("ignore-all-space"): bool,
+    Optional("bim"): str,
+    Optional("iasmesh"): str,
+    Optional("capture"): str,
+    Optional("boaq"): str,
+    Optional("ehren"):int,
+    Optional("feynman"): bool,
+    Optional("iasprops"): bool,
+    Optional("magprops"): str,
+    Optional("source"): bool,
+    Optional("iaswrite"): bool,
+    Optional("atidsprop"): str,
+    Optional("encomp"): int,
+    Optional("warn"): bool,
+    Optional("scp"): str,
+    Optional("delmog"): bool,
+    Optional("skipint"): bool,
+    Optional("f2w"): str,
+    Optional("f2wonly"): bool,
+    Optional("atoms"): str,
+    Optional("mir"): float,
+    Optional("cpconn"):  str,
+    Optional("intveeaa"): str,
+    Optional("atlaprhocps"): bool,
+    Optional("wsp"): bool,
+    Optional("nprocs"): int,
+    Optional("naat"): int,
+    Optional("shm_lmax"): int,
+    Optional("maxmem"): int,
+    Optional("verifyw"): str,
+    Optional("saw"): bool,
+    Optional("autonnacps"): bool,
+    # Optional("help"), commented, these are options
+    # Optional("console"),
+    # Optional("newconsole"),
+    # Optional("run"),
+    # Optional("nogui"),
 }
 
-
-class DiffParameters(Dict):  # pylint: disable=too-many-ancestors
+class AimqbParameters(Dict): # pylint: disable=too-many-ancestors
     """
-    Command line options for diff.
-
-    This class represents a python dictionary used to
+    Command line options for aimqb.
+    
+    This class represents a python dictionary used to 
     pass command line options to the executable.
     """
-
-    # "voluptuous" schema  to add automatic validation
     schema = Schema(cmdline_options)
-
-    # pylint: disable=redefined-builtin
-    def __init__(self, dict=None, **kwargs):
+    def __init__(self,dict=None,**kwargs):
         """
         Constructor for the data class
 
-        Usage: ``DiffParameters(dict{'ignore-case': True})``
+        Usage: ``AimqbParameters(dict{'ignore-case': True})``
 
         :param parameters_dict: dictionary with commandline parameters
         :param type parameters_dict: dict
 
         """
-        dict = self.validate(dict)
+        dict=self.validate(dict)
         super().__init__(dict=dict, **kwargs)
-
+    
     def validate(self, parameters_dict):
         """Validate command line options.
 
@@ -55,17 +81,15 @@ class DiffParameters(Dict):  # pylint: disable=too-many-ancestors
         :param type parameters_dict: dict
         :returns: validated dictionary
         """
-        return DiffParameters.schema(parameters_dict)
+        return AimqbParameters.schema(parameters_dict)
 
-    def cmdline_params(self, file1_name, file2_name):
+    def cmdline_params(self, file_name):
         """Synthesize command line parameters.
 
-        e.g. [ '--ignore-case', 'filename1', 'filename2']
+        e.g. [ '-atlaprhocps', 'filename']
 
-        :param file_name1: Name of first file
-        :param type file_name1: str
-        :param file_name2: Name of second file
-        :param type file_name2: str
+        :param file_name: Name of first file
+        :param type file_name: str
 
         """
         parameters = []
@@ -73,9 +97,9 @@ class DiffParameters(Dict):  # pylint: disable=too-many-ancestors
         pm_dict = self.get_dict()
         for option, enabled in pm_dict.items():
             if enabled:
-                parameters += ["--" + option]
+                parameters += ["-" + option]
 
-        parameters += [file1_name, file2_name]
+        parameters += [file_name]
 
         return [str(p) for p in parameters]
 
@@ -91,3 +115,76 @@ class DiffParameters(Dict):  # pylint: disable=too-many-ancestors
         string = super().__str__()
         string += "\n" + str(self.get_dict())
         return string
+    
+# class DiffParameters(Dict):  # pylint: disable=too-many-ancestors
+#     """
+#     Command line options for diff.
+
+#     This class represents a python dictionary used to
+#     pass command line options to the executable.
+#     """
+
+#     # "voluptuous" schema  to add automatic validation
+#     schema = Schema(cmdline_options)
+
+#     # pylint: disable=redefined-builtin
+#     def __init__(self, dict=None, **kwargs):
+#         """
+#         Constructor for the data class
+
+#         Usage: ``DiffParameters(dict{'ignore-case': True})``
+
+#         :param parameters_dict: dictionary with commandline parameters
+#         :param type parameters_dict: dict
+
+#         """
+#         dict = self.validate(dict)
+#         super().__init__(dict=dict, **kwargs)
+
+#     def validate(self, parameters_dict):
+#         """Validate command line options.
+
+#         Uses the voluptuous package for validation. Find out about allowed keys using::
+
+#             print(DiffParameters).schema.schema
+
+#         :param parameters_dict: dictionary with commandline parameters
+#         :param type parameters_dict: dict
+#         :returns: validated dictionary
+#         """
+#         return DiffParameters.schema(parameters_dict)
+
+#     def cmdline_params(self, file1_name, file2_name):
+#         """Synthesize command line parameters.
+
+#         e.g. [ '--ignore-case', 'filename1', 'filename2']
+
+#         :param file_name1: Name of first file
+#         :param type file_name1: str
+#         :param file_name2: Name of second file
+#         :param type file_name2: str
+
+#         """
+#         parameters = []
+
+#         pm_dict = self.get_dict()
+#         for option, enabled in pm_dict.items():
+#             if enabled:
+#                 parameters += ["-" + option]
+
+#         parameters += [file1_name, file2_name]
+
+#         return [str(p) for p in parameters]
+
+#     def __str__(self):
+#         """String representation of node.
+
+#         Append values of dictionary to usual representation. E.g.::
+
+#             uuid: b416cbee-24e8-47a8-8c11-6d668770158b (pk: 590)
+#             {'ignore-case': True}
+
+#         """
+#         string = super().__str__()
+#         string += "\n" + str(self.get_dict())
+#         return string
