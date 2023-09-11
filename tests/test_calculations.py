9533e15e5c8cc633fa2a154df53842f373c4be10
kmlefran <kgagnon@lakeheadu.ca>
2023-09-11 15:56:57 -0400

    Test test

--- tests/test_calculations.py
+++ tests/test_calculations.py
@@ -2,13 +2,14 @@
 import os
 
 from aiida.engine import run
-from aiida.orm import Dict, SinglefileData
+from aiida.orm import Dict, SinglefileData, load_code
 from aiida.plugins import CalculationFactory, DataFactory
 
 from . import TEST_DIR
 
 
-def test_process(aimall_code):
+# def test_process(aimall_code):
+def test_process():
     """Test running a calculation
     note this does not test that the expected outputs are created of output parsing"""
 
@@ -19,10 +20,11 @@ def test_process(aimall_code):
     file = SinglefileData(
         file=os.path.join(TEST_DIR, "input_files", "water_wb97xd_augccpvtz_qtaim.wfx")
     )
-
+    code = load_code("aimall2")
     # set up calculation
     inputs = {
-        "code": aimall_code,
+        # "code": aimall_code,
+        "code": code,
         "parameters": parameters,
         "file": file,
     }
