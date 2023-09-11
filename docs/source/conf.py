eef110739a1a26d6ca8aa9fe27cffd89775e5e76
kmlefran <kgagnon@lakeheadu.ca>
2023-09-11 15:34:59 -0400

    Docs trial

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -16,6 +16,7 @@ import time
 
 from aiida.manage.configuration import load_documentation_profile
 
+sys.path.insert(0, "/Users/chemlab/anaconda3/bin")
 import aiida_aimall
 
 # -- AiiDA-related setup --------------------------------------------------
@@ -28,7 +29,7 @@ load_documentation_profile()
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
-sys.path.insert(0, "/Users/chemlab/anaconda3/bin")
+
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
