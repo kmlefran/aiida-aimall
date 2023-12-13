895332639877cb859f090e9eed4ce45dacd50004
kmlefran <kgagnon@lakeheadu.ca>
2023-12-13 13:48:46 -0500

    test test

--- tests/calculations/test_aimqbcalculation.py
+++ tests/calculations/test_aimqbcalculation.py
@@ -1,19 +1,29 @@
 """tests for AimqbCalculation"""
 
+import os
+
 from aiida.common import datastructures
 from aiida.orm import SinglefileData
 
 from aiida_aimall.data import AimqbParameters
 
 
-def test_aimqb_default(fixture_sandbox, generate_calc_job, fixture_code):
+def test_aimqb_default(
+    fixture_sandbox, generate_calc_job, fixture_code, filepath_tests
+):
     """Tests that an aimcalculation can be instantiated"""
     entry_point_name = "aimall"
     parameters = AimqbParameters({"naat": 2, "nproc": 2, "atlaprhocps": True})
     inputs = {
         "code": fixture_code("aimall"),
         "parameters": parameters,
-        "file": SinglefileData("inputs/water_wb97xd_augccpvtz_qtaim"),
+        "file": SinglefileData(
+            os.path.join(
+                filepath_tests(),
+                "calculations/inputs",
+                "water_wb97xd_augccpvtz_qtaim.wfx",
+            )
+        ),
         "metadata": {
             "options": {
                 "resources": {"num_machines": 1, "num_mpiprocs_per_machine": 2},
