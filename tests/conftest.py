10e81b31253e81ef4e64e3da87468681be3cb7ec
kmlefran <kgagnon@lakeheadu.ca>
2023-12-14 13:59:36 -0500

    Parser testing first draft

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,12 +1,14 @@
 # pylint: disable=redefined-outer-name,too-many-statements
 """Initialise a text database and profile for pytest."""
-# import io
+import io
 import os
+
+# import pathlib
+import shutil
+from collections.abc import Mapping
 
 import pytest
 
-# import pathlib
-# import shutil
 # import tempfile
 # from collections.abc import Mapping
 
@@ -96,3 +98,150 @@ def generate_calc_job():
         return calc_info
 
     return _generate_calc_job
+
+
+@pytest.fixture(scope="session")
+def generate_parser():
+    """Fixture to load a parser class for testing parsers."""
+
+    def _generate_parser(entry_point_name):
+        """Fixture to load a parser class for testing parsers.
+
+        :param entry_point_name: entry point name of the parser class
+        :return: the `Parser` sub class
+        """
+        from aiida.plugins import ParserFactory
+
+        return ParserFactory(entry_point_name)
+
+    return _generate_parser
+
+
+@pytest.fixture
+def generate_calc_job_node(fixture_localhost):
+    """Generate a mock `CalcJobNode` for testing parsers"""
+
+    def flatten_inputs(inputs, prefix=""):
+        flat_inputs = []
+        for key, value in inputs.items():
+            if isinstance(value, Mapping):
+                flat_inputs.extend(flatten_inputs(value, prefix=prefix + key + "__"))
+            else:
+                flat_inputs.append((prefix + key, value))
+        return flat_inputs
+
+    def _generate_calc_job_node(  # pylint:disable=too-many-arguments
+        entry_point_name="base",
+        computer=None,
+        test_name=None,
+        inputs=None,
+        attributes=None,
+        retrieve_temporary=None,
+    ):
+        """Fixture to generate a mock `CalcJobNode` for testing parsers.
+
+        :param entry_point_name: entry point name of the calculation class
+        :param computer: a `Computer` instance
+        :param test_name: relative path of directory with test output files in the `fixtures/{entry_point_name}` folder.
+        :param inputs: any optional nodes to add as input links to the corrent CalcJobNode
+        :param attributes: any optional attributes to set on the node
+        :param retrieve_temporary: optional tuple of an absolute filepath of a temporary directory and a list of
+            filenames that should be written to this directory, which will serve as the `retrieved_temporary_folder`.
+            For now this only works with top-level files and does not support files nested in directories.
+        :return: `CalcJobNode` instance with an attached `FolderData` as the `retrieved` node.
+        """
+        # pylint:disable=too-many-locals too-many-branches
+        from aiida import orm
+        from aiida.common import LinkType
+        from aiida.plugins.entry_point import format_entry_point_string
+
+        if computer is None:
+            computer = fixture_localhost
+
+        filepath_folder = None
+
+        if test_name is not None:
+            basepath = os.path.dirname(os.path.abspath(__file__))
+            filename = os.path.join(entry_point_name, test_name)
+            filepath_folder = os.path.join(basepath, "parsers", "fixtures", filename)
+            filepath_input = os.path.join(filepath_folder, "aiida.in")
+
+        entry_point = format_entry_point_string("aiida.calculations", entry_point_name)
+
+        node = orm.CalcJobNode(computer=computer, process_type=entry_point)
+        node.base.attributes.set("input_filename", "aiida.in")
+        node.base.attributes.set("output_filename", "aiida.out")
+        node.base.attributes.set("error_filename", "aiida.err")
+        node.set_option("resources", {"num_machines": 1, "num_mpiprocs_per_machine": 1})
+        node.set_option("max_wallclock_seconds", 1800)
+
+        if attributes:
+            node.base.attributes.set_many(attributes)
+
+        if filepath_folder:
+
+            try:
+                with open(filepath_input, encoding="utf-8") as input_file:
+                    wfxstr = input_file.readlines()
+                    strtoencode = ""
+                    for line in wfxstr:
+                        strtoencode = strtoencode + line
+            except FileNotFoundError:
+                pass
+            else:
+                inputs["structure"] = orm.SinglefileData(
+                    io.BytesIO(strtoencode.encode())
+                )
+
+        if inputs:
+            metadata = inputs.pop("metadata", {})
+            options = metadata.get("options", {})
+
+            for name, option in options.items():
+                node.set_option(name, option)
+
+            for link_label, input_node in flatten_inputs(inputs):
+                input_node.store()
+                node.base.links.add_incoming(
+                    input_node, link_type=LinkType.INPUT_CALC, link_label=link_label
+                )
+
+        node.store()
+
+        if retrieve_temporary:
+            dirpath, filenames = retrieve_temporary
+            for filename in filenames:
+                try:
+                    shutil.copy(
+                        os.path.join(filepath_folder, filename),
+                        os.path.join(dirpath, filename),
+                    )
+                except FileNotFoundError:
+                    pass  # To test the absence of files in the retrieve_temporary folder
+
+        if filepath_folder:
+            retrieved = orm.FolderData()
+            retrieved.base.repository.put_object_from_tree(filepath_folder)
+
+            # Remove files that are supposed to be only present in the retrieved temporary folder
+            if retrieve_temporary:
+                for filename in filenames:
+                    try:
+                        retrieved.base.repository.delete_object(filename)
+                    except OSError:
+                        pass  # To test the absence of files in the retrieve_temporary folder
+
+            retrieved.base.links.add_incoming(
+                node, link_type=LinkType.CREATE, link_label="retrieved"
+            )
+            retrieved.store()
+
+            remote_folder = orm.RemoteData(computer=computer, remote_path="/tmp")
+            remote_folder.base.links.add_incoming(
+                node, link_type=LinkType.CREATE, link_label="remote_folder"
+            )
+            remote_folder.store()
+
+        return node
+
+    return _generate_calc_job_node
---
+++ tests/parsers/test_aimqbparser.py
@@ -0,0 +1,61 @@
+# pylint: disable=redefined-outer-name
+"""Tests for the `AimqbParser"""
+
+import os
+
+import pytest
+from aiida import orm
+from aiida.common import AttributeDict
+from aiida.orm import SinglefileData
+
+from aiida_aimall.data import AimqbParameters
+
+
+@pytest.fixture
+def generate_aimqb_inputs():
+    """Generates inputs of a default aimqb calculation"""
+
+    def _generate_aimqb_inputs(fixture_code, filepath_tests):
+        """Return only those inputs the parser will expect to be there"""
+        parameters = AimqbParameters({"naat": 2, "nproc": 2})
+        inputs = {
+            "code": fixture_code("aimall"),
+            "parameters": parameters,
+            "file": SinglefileData(
+                os.path.join(
+                    filepath_tests,
+                    "parsers/inputs",
+                    "h2_opt.wfx",
+                )
+            ),
+            "metadata": {
+                "options": {
+                    "resources": {"num_machines": 1, "num_mpiprocs_per_machine": 2},
+                }
+            },
+        }
+        return AttributeDict(inputs)
+
+    return _generate_aimqb_inputs
+
+
+def test_aimqb_parser_default(
+    fixture_localhost, generate_calc_job_node, generate_parser, generate_aimqb_inputs
+):
+    """Test an aimqb calculation"""
+    name = "default"
+    entry_point_calc_job = "aimall"
+    entry_point_parser = "aimqb.base"
+    node = generate_calc_job_node(
+        entry_point_calc_job, fixture_localhost, name, generate_aimqb_inputs
+    )
+    parser = generate_parser(entry_point_parser)
+    results, calcfunction = parser.parse_from_node(node, store_provenance=False)
+
+    assert calcfunction.is_finished, calcfunction.exception
+    assert calcfunction.is_finished_ok, calcfunction.exit_message
+    assert not orm.Log.collection.get_logs_for(node), [
+        log.message for log in orm.Log.collection.get_logs_for(node)
+    ]
+    assert "atomic_properties" in results
+    assert "bcp_properties" in results
