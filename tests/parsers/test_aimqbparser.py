6bc552d7cea8bfd2377a097962b936be96217d4e
kmlefran <kgagnon@lakeheadu.ca>
2023-12-14 14:04:57 -0500

    Call generate_aimqb_inputs

--- tests/parsers/test_aimqbparser.py
+++ tests/parsers/test_aimqbparser.py
@@ -47,7 +47,7 @@ def test_aimqb_parser_default(
     entry_point_calc_job = "aimall"
     entry_point_parser = "aimqb.base"
     node = generate_calc_job_node(
-        entry_point_calc_job, fixture_localhost, name, generate_aimqb_inputs
+        entry_point_calc_job, fixture_localhost, name, generate_aimqb_inputs()
     )
     parser = generate_parser(entry_point_parser)
     results, calcfunction = parser.parse_from_node(node, store_provenance=False)
