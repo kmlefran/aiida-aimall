ae065e6484e5868bef08597d8d7fdb7a1c3be0ad
kmlefran <kgagnon@lakeheadu.ca>
2023-12-15 13:11:30 -0500

    Modify test documentation

--- tests/workchains/test_aimreorworkchain.py
+++ tests/workchains/test_aimreorworkchain.py
@@ -3,7 +3,7 @@ from plumpy.utils import AttributesFrozendict
 
 
 def test_setup(generate_workchain_aimreor):
-    """Test `AimReorWorkChain.setup`."""
+    """Test creation of `AimReorWorkChain`."""
     process = generate_workchain_aimreor()
 
     assert isinstance(process.inputs, AttributesFrozendict)
